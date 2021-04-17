dfc5c8ba9567da62097efa1366015f3a6d5be90e
Daniel Fu <danielfu123@gmail.com>
2021-04-17 06:03:40 -0700

    add round button ui

---
+++ comps/RoundButton/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import {useRouter} from 'next/router';
+
+const RoundButtonCont = styled.span`
+
+`;
+
+const RoundButtonInput = styled.button`
+    display: flex;
+    width: 105px;
+    height: 105px;
+    border-radius: 50px;
+    font-weight: bold;
+    justify-content: center;
+    align-items: center;
+    color:white;
+    background-color: ${props=>props.bgcolor};
+    font-size: ${props=>props.fontSize}px;
+    border: none;
+`;
+
+//props
+const RoundButtonUI = ({
+    text="BEGIN",
+    bgcolor="#9AC397",
+    routeTo="/",
+    fontSize=30
+}) => {
+    const router = useRouter();
+    return <RoundButtonCont onClick={()=>router.push(routeTo)}>
+        <RoundButtonInput bgcolor={bgcolor} fontSize={fontSize}>
+        {text}
+        </RoundButtonInput>
+    </RoundButtonCont>
+}
+
+export default RoundButtonUI;
