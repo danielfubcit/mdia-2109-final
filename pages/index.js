a2244b929a148cb9166e6ec86de37d797adf9dc1
Daniel Fu <danielfu123@gmail.com>
2021-04-17 21:21:59 -0700

    adding index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from 'styled-components';
+import React, {useState}  from 'react';
+import ImageUI from '../comps/Image';
+import MainTitleUI from '../comps/MainTitle';
+import RoundButtonUI from '../comps/RoundButton';
+
+const HomeCont = styled.div`
+  margin:0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #79CFE2;
+
+  .img, .title, .button{
+    margin:50px;
+  }
+`;
 
 export default function Home() {
-  return (
-    <div>
-      Home Page
+  return <HomeCont>
+    <Head>
+      <title>Home Page</title>
+    </Head>
+    <div className="img">
+      <ImageUI src="/appLogo.svg" width="100"></ImageUI>
     </div>
-  )
+    <div className="title">
+      <MainTitleUI fontsize="24" color="white" text="TEST YOUR CARBON EMISSION RATE"></MainTitleUI>
+    </div>
+    <div className="button">
+      <RoundButtonUI></RoundButtonUI>
+    </div>
+  </HomeCont>
 }
