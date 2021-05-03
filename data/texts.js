984f47433f265ca686372d7c0c0012c4cce1cfa3
Daniel Fu <danielfu123@gmail.com>
2021-05-03 15:08:40 -0700

    update text data

--- data/texts.js
+++ data/texts.js
@@ -10,7 +10,7 @@ export const vehicles = {
         route3:"/distance/electric/fifty",
     },
     hybrid:{
-        route1:"/distance/hybrid/0",
+        route1:"/distance/hybrid/zero",
         route2:"/distance/hybrid/twentyFive",
         route3:"/distance/hybrid/fifty",
     }
@@ -19,38 +19,47 @@ export const vehicles = {
 export const gaskm = {
     zero:{
         text1:"4kg",
+        route:"/distance/gas/zero/fact",
     },
     twentyFive:{
         text1:"10kg",
+        route:"/distance/gas/twentyFive/fact",
     },
     fifty:{
         text1:"14kg",
         bgcolor:"#FA9AAB",
+        route:"/distance/gas/fifty/fact",
     }
 }
 
 export const electrickm = {
     zero:{
         text1:"1kg",
+        route:"/distance/electric/zero/fact",
     },
     twentyFive:{
         text1:"2kg",
+        route:"/distance/electric/twentyFive/fact",
     },
     fifty:{
         text1:"3kg",
         bgcolor:"#FA9AAB",
+        route:"/distance/electric/fifty/fact",
     }
 }
 
 export const hybridkm = {
     zero:{
         text1:"3kg",
+        route:"/distance/hybrid/zero/fact",
     },
     twentyFive:{
         text1:"7kg",
+        route:"/distance/hybrid/twentyFive/fact",
     },
     fifty:{
         text1:"10kg",
         bgcolor:"#FA9AAB",
+        route:"/distance/hybrid/fifty/fact",
     }
 }
