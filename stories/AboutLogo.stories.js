d3ec6ba8efd26068cc5ec1af45f2f91834edab5f
Daniel Fu <danielfu123@gmail.com>
2021-05-10 15:11:13 -0700

    add comp to storybook

---
+++ stories/AboutLogo.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AboutLogoUI from '../comps/AboutLogo';
+
+export default {
+    title:"Example/AboutLogo",
+    component:<AboutLogoUI />
+}
+
+export const DefaultAboutLogo = () => <AboutLogoUI />
